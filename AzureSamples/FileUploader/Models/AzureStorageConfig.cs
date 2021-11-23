72293e5dd11a222263201b0cdd05ec6542abcf17
Santosh Mali <santoshmalisp@gmail.com>
2021-11-23 22:53:31 +0530

    file upload

---
+++ AzureSamples/FileUploader/Models/AzureStorageConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUploader.Models
+{
+    public class AzureStorageConfig
+    {
+        public string ConnectionString { get; set; }
+        public string FileContainerName { get; set; }
+    }
+}
